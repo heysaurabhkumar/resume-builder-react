34e665f37f5d68321429b6a38e01d737c2c558e5
thesaurabh4714 <42849052+thesaurabh4714@users.noreply.github.com>
2021-10-22 12:15:21 +0530

    resume form

--- src/components/resumes/ResumeForm.js
+++ src/components/resumes/ResumeForm.js
@@ -1,156 +1,221 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ResumeForm() {
+  const [resumeForm, setResumeForm] = useState({
+    fullname: "",
+    position: "",
+    email: "",
+    mobile: "",
+    address: "",
+    skills: "",
+    profile: "",
+    linkedin: "",
+    facebook: "",
+    instagram: "",
+    languages: "",
+    objective: "",
+    experience: "",
+    project: "",
+    certification: "",
+    education: "",
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setResumeForm({ ...resumeForm, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(resumeForm);
+  };
   return (
     <>
       <div className="container">
         <div className="container text-center mt-2">
           <h1>Resume Builder</h1>
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-md-6 mt-2">
               <div className="form-group mt-3">
-                <label className="" htmlFor="fullNameInput">
+                <label className="" htmlFor="fullname">
                   Full Name
                 </label>
                 <input
                   type="text"
-                  id="fullNameInput"
+                  id="fullname"
+                  name="fullname"
                   className="form-control"
                   placeholder="Enter full name"
+                  value={resumeForm.fullname}
+                  onChange={handleChange}
                 />
               </div>
 
               <div className="form-group mt-2">
-                <label className="" htmlFor="positionInput">
+                <label className="" htmlFor="position">
                   Current Position
                 </label>
                 <input
                   type="text"
-                  id="positionInput"
+                  id="position"
+                  name="position"
                   className="form-control"
                   placeholder="Enter current position"
+                  value={resumeForm.position}
+                  onChange={handleChange}
                 />
               </div>
 
               <div className="form-group mt-2">
-                <label className="" htmlFor="emailInput">
+                <label className="" htmlFor="email">
                   Email
                 </label>
                 <input
                   type="email"
-                  id="emailInput"
+                  id="email"
+                  name="email"
                   className="form-control"
                   placeholder="Enter email"
+                  value={resumeForm.email}
+                  onChange={handleChange}
                 />
               </div>
               <div className="form-group mt-2">
-                <label className="" htmlFor="mobileInput">
+                <label className="" htmlFor="mobile">
                   Mobile No
                 </label>
                 <input
                   type="text"
-                  id="mobileInput"
+                  id="mobile"
+                  name="mobile"
                   className="form-control"
                   placeholder="Enter mobile number"
+                  value={resumeForm.mobile}
+                  onChange={handleChange}
                 />
               </div>
               <div className="form-group mt-2">
-                <label className="" htmlFor="addressInput">
+                <label className="" htmlFor="address">
                   Address
                 </label>
                 <textarea
-                  id="addressInput"
+                  id="address"
+                  name="address"
                   rows="3"
                   className="form-control"
                   placeholder="Enter address"
+                  value={resumeForm.address}
+                  onChange={handleChange}
                 ></textarea>
               </div>
 
               <div className="form-group mt-2">
-                <label className="" htmlFor="skillsInput">
+                <label className="" htmlFor="skills">
                   Skills
                 </label>
                 <textarea
-                  id="skillsInput"
+                  id="skills"
+                  name="skills"
                   rows="2"
                   className="form-control"
                   placeholder="Write your skills comma seperated"
+                  value={resumeForm.skills}
+                  onChange={handleChange}
                 ></textarea>
               </div>
               <div className="form-group mt-2">
-                <label className="" htmlFor="profileInput">
+                <label className="" htmlFor="profile">
                   Enter your picture url
                 </label>
                 <input
-                  type="url"
-                  id="profileInput"
+                  type="text"
+                  id="profile"
+                  name="profile"
                   placeholder="Write your profile url"
                   className="form-control"
+                  value={resumeForm.profile}
+                  onChange={handleChange}
                 />
               </div>
               <p className="text-secondary mt-3">Social Links</p>
               <div className="form-group mt-2">
-                <label className="" htmlFor="linkedInput">
+                <label className="" htmlFor="linkedin">
                   LinkedIn
                 </label>
                 <input
                   type="text"
-                  id="linkedInput"
+                  id="linkedin"
+                  name="linkedin"
                   className="form-control"
                   placeholder="Enter linkedin profile url"
+                  value={resumeForm.linkedin}
+                  onChange={handleChange}
                 />
               </div>
               <div className="form-group mt-2">
-                <label className="" htmlFor="facebookInput">
+                <label className="" htmlFor="facebook">
                   Facebook
                 </label>
                 <input
                   type="text"
-                  id="facebookInput"
+                  id="facebook"
+                  name="facebook"
                   className="form-control"
                   placeholder="Enter facebook profile url"
+                  value={resumeForm.facebook}
+                  onChange={handleChange}
                 />
               </div>
               <div className="form-group mt-2">
-                <label className="" htmlFor="instagramInput">
+                <label className="" htmlFor="instagram">
                   Instagram
                 </label>
                 <input
                   type="text"
-                  id="instagramInput"
+                  id="instagram"
+                  name="instagram"
                   className="form-control"
                   placeholder="Enter instagram profile url"
+                  value={resumeForm.instagram}
+                  onChange={handleChange}
                 />
               </div>
               <div className="form-group mt-2">
-                <label className="" htmlFor="languageInput">
+                <label className="" htmlFor="languages">
                   Languages Speak
                 </label>
                 <textarea
-                  id="languageInput"
+                  id="languages"
+                  name="languages"
                   rows="1"
                   className="form-control"
                   placeholder="Write your languages comma seperated"
+                  value={resumeForm.languages}
+                  onChange={handleChange}
                 ></textarea>
               </div>
 
               <div className="form-group mt-2">
-                <label className="" htmlFor="objectiveInput">
+                <label className="" htmlFor="objective">
                   Objective
                 </label>
                 <textarea
-                  id="objectiveInput"
+                  id="objective"
+                  name="objective"
                   rows="4"
                   className="form-control"
                   placeholder="Write your objective"
+                  value={resumeForm.objective}
+                  onChange={handleChange}
                 ></textarea>
               </div>
             </div>
             <div className="col-md-6 mt-2">
               <div className="form-group mt-3">
-                <label className="" htmlFor="experienceInput">
+                <label className="" htmlFor="experience">
                   Experience
                 </label>
                 <textarea
